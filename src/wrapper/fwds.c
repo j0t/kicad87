c89967cf8d19adeac6b7602f92d8e4bd797e1f35
j0t <1532807+j0t@users.noreply.github.com>
2024-10-01 21:57:43 +0300

    cleanup

--- src/wrapper/fwds.c
+++
@@ -1,5 +0,0 @@
-//------------------------------
-asm (".section .drectve");
-asm (".ascii \" -export:ProcessId=test.ProcessId\"");
-asm (".ascii \" -export:OpenFileUtil=test.OpenFileUtil\"");
-//------------------------------
